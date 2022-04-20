0d86b0855b919ac84b98da9373e54b5333412748
yamamoto <skacjswo1234@github.com>
2022-04-20 19:45:10 +0900

    push nam2

---
+++ domain/src/main/java/com/uplus/domain/login/service/NamService.java
@@ -0,0 +1,9 @@
+package com.uplus.domain.yamamoto.service;
+
+import com.uplus.domain.yamamoto.dto.NamDTO;
+
+import java.util.List;
+
+public interface NamService {
+    List<NamDTO> selectNam();
+}
